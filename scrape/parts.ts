d51b3518a3f4d58700ecfc120a682579b7dd25db
Britnell <tommy.britnell@gmail.com>
2024-03-01 21:22:01 +0100

    scraper scripts

---
+++ scrape/parts.ts
@@ -0,0 +1,31 @@
+import * as cheerio from "cheerio";
+import { collectionsHtml } from "./test";
+
+export const listCollections = () => {
+  const $ = cheerio.load(collectionsHtml);
+  const list: any[] = [];
+  $("#collection-select")
+    .children()
+    .each((x, el) => {
+      const li = $(el).find("a");
+      const href = li.attr("href");
+      const name = li.text();
+      list.push({ href, name });
+    });
+  return list;
+};
+
+export const getPageFiles = (html: string) => {
+  const $ = cheerio.load(html);
+  const list: object[] = [];
+  $("ul.preview-list")
+    .children()
+    .each((x, el) => {
+      //   if (x > 10) return;
+      const li = $(el);
+      const href = li.find("a:contains('details')").attr("href");
+      const name = li.find("h4").text();
+      list.push({ href, name });
+    });
+  return list;
+};
---
+++ scrape/scraper.ts
@@ -0,0 +1,72 @@
+import { getPageFiles, listCollections } from "./parts";
+import { pageHtml } from "./test";
+import { $ } from "bun";
+
+const collections = listCollections();
+console.log(collections.map((c, x) => ({ ...c, x })));
+
+// const samples = getPageFiles(pageHtml);
+// console.log(samples.length);
+
+const loclabs = "https://citizen-dj.labs.loc.gov";
+
+const getPage = async (x: number) => {
+  const url = loclabs + collections[x].href;
+  const page = await fetch(url).then((res) => res.text());
+  const files = getPageFiles(page);
+  console.log(x, url, files.length);
+
+  const file = Bun.file(`./scrape/${collections[x].name}.txt`);
+  await Bun.write(file, JSON.stringify(files));
+};
+
+// for (let x = 0; x < collections.length; x++) {
+//   getPage(x);
+//   await delay(2000);
+// }
+
+function delay(x: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(null), x);
+  });
+}
+
+const getFileList = async (name: string) => {
+  const file = Bun.file(name);
+  const str = await file.text();
+  try {
+    const data = JSON.parse(str);
+    return data;
+  } catch (e) {
+    return [];
+  }
+};
+
+const combine = async () => {
+  const lists = await Promise.all(txts.map((txt) => getFileList(txt)));
+  const combine = lists.reduce((t, list) => [...t, ...list], []);
+  const file = Bun.file(`./scrape/all.txt`);
+  await Bun.write(file, JSON.stringify(combine));
+
+  //   const xlist = await getFileList(txts[x]);
+};
+
+const txts = [
+  "./scrape/American English Dialect Recordings.txt",
+  "./scrape/The National Jukebox: Jazz.txt",
+  "./scrape/Free Music Archive.txt",
+  "./scrape/The National Jukebox: Musical Theater.txt",
+  "./scrape/Inventing Entertainment.txt",
+  "./scrape/The National Jukebox: Opera.txt",
+  "./scrape/MusicBox Project.txt",
+  "./scrape/The National Jukebox: Popular Music.txt",
+  "./scrape/The Joe Smith Collection.txt",
+  "./scrape/The National Screening Room.txt",
+  "./scrape/The National Jukebox: Blues.txt",
+  "./scrape/Tony Schwartz Collection.txt",
+  "./scrape/The National Jukebox: Classical Music.txt",
+  "./scrape/Variety Stage Sound Recordings and Motion Pictures.txt",
+  "./scrape/The National Jukebox: Folk Music.txt",
+];
+
+combine();
