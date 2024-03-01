dfa64292640f138754875c0c8024ecd36ae41ac4
Britnell <tommy.britnell@gmail.com>
2024-03-01 21:29:17 +0100

    prepend loc when necssary

--- src/pages/api/random.ts
+++ src/pages/api/random.ts
@@ -1,19 +1,23 @@
 import type { APIRoute } from "astro";
 import list from "../../../scrape/all.json";
 
+const loclabs = "https://citizen-dj.labs.loc.gov";
+
 export const GET: APIRoute = () => {
   const r = Math.floor(list.length * Math.random());
-  const li = list[r];
+  const { href } = list[r];
+  const isFull = href.startsWith("http");
+  const link = isFull ? href : loclabs + href;
 
   return new Response("", {
     status: 302,
     headers: {
-      Location: li.href,
+      Location: link,
     },
   });
-  return new Response(JSON.stringify(li), {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  // return new Response(JSON.stringify(li), {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
 };
